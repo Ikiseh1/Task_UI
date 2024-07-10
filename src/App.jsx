f4dcba9a89e69c4aa95a17e1e377cef111c87f5c
Ikiseh Samuel <ikisehtochukwu@gmail.com>
2024-07-10 22:57:10 +0100

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ import GetStarted from './HOMEPAGE/GetStarted/GetStarted.jsx';
 import Testimonials from './HOMEPAGE/Testimonials/Testimonials.jsx';
 import Pricing from './HOMEPAGE/Pricing/Pricing.jsx';
 import FAQ from './HOMEPAGE/FAQ/FAQ.jsx';
+// import Footer from './HOMEPAGE/Footer/Footer.jsx';
+
 import './App.css'
 
 function App() {
   return (
     <div >
+      <h1>Gsrdgf</h1>
       <Navbar />
       <Hero />
       <Features />
@@ -20,6 +23,7 @@ function App() {
       <Testimonials />
       <Pricing />
       <FAQ />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/HOMEPAGE/Testimonials/Testimonials.jsx
+++ src/HOMEPAGE/Testimonials/Testimonials.jsx
@@ -1,68 +1,68 @@
-import './Testimonials.css'
+ import './Testimonials.css'
 
 function Testimonials() {
-  return (
-    <section className="" id="testimonial">
-      <div className="testimonial__container">
-        <h1>Loved by businesses worldwide.</h1>
-        <p>Our software is so simple that people can’t help but fall in love with it. Simplicity is easy when you just skip tons of mission-critical features</p>
-        <div className="testimonial__wrapper">
-          <div className="testimonial__card">
-            <i className='bx bxs-quote-alt-left blockquote__icon'></i>
-            <blockquote className="testimonial__blockquote">
-              <p>TaxPal is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.</p>
-            </blockquote>
-            <div className="testimonial__info">
-              <div>
-                <h2>Sheryl Berge</h2>
-                <p>CEO at Lynch LLC</p>
+    return (
+      <section className="" id="testimonial">
+        <div className="testimonial__container">
+          <h1>Loved by businesses worldwide.</h1>
+          <p>Our software is so simple that people can’t help but fall in love with it. Simplicity is easy when you just skip tons of mission-critical features</p>
+          <div className="testimonial__wrapper">
+            <div className="testimonial__card">
+              <i className='bx bxs-quote-alt-left blockquote__icon'></i>
+              <blockquote className="testimonial__blockquote">
+                <p>TaxPal is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.</p>
+              </blockquote>
+              <div className="testimonial__info">
+                <div>
+                  <h2>Sheryl Berge</h2>
+                  <p>CEO at Lynch LLC</p>
+                </div>
+                <img src="image/avatars/avatar-1.png" alt="Sheryl Berge" />
               </div>
-              <img src="image/avatars/avatar-1.png" alt="Sheryl Berge" />
             </div>
-          </div>
-          <div className="testimonial__card">
-            <i className='bx bxs-quote-alt-left blockquote__icon'></i>
-            <blockquote className="testimonial__blockquote">
-              <p>TaxPal is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.</p>
-            </blockquote>
-            <div className="testimonial__info">
-              <div>
-                <h2>Russell White</h2>
-                <p>Director at Acme</p>
+            <div className="testimonial__card">
+              <i className='bx bxs-quote-alt-left blockquote__icon'></i>
+              <blockquote className="testimonial__blockquote">
+                <p>TaxPal is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.</p>
+              </blockquote>
+              <div className="testimonial__info">
+                <div>
+                  <h2>Russell White</h2>
+                  <p>Director at Acme</p>
+                </div>
+                <img src="image/avatars/avatar-2.png" alt="Russell White" />
               </div>
-              <img src="image/avatars/avatar-2.png" alt="Russell White" />
             </div>
-          </div>
-          <div className="testimonial__card">
-            <i className='bx bxs-quote-alt-left blockquote__icon'></i>
-            <blockquote className="testimonial__blockquote">
-              <p>TaxPal is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.</p>
-            </blockquote>
-            <div className="testimonial__info">
-              <div>
-                <h2>Denzel Schmidt</h2>
-                <p>CEO at Lynch LLC</p>
+            <div className="testimonial__card">
+              <i className='bx bxs-quote-alt-left blockquote__icon'></i>
+              <blockquote className="testimonial__blockquote">
+                <p>TaxPal is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.</p>
+              </blockquote>
+              <div className="testimonial__info">
+                <div>
+                  <h2>Denzel Schmidt</h2>
+                  <p>CEO at Lynch LLC</p>
+                </div>
+                <img src="image/avatars/avatar-3.png" alt="Denzel Schmidt" />
               </div>
-              <img src="image/avatars/avatar-3.png" alt="Denzel Schmidt" />
             </div>
-          </div>
-          <div className="testimonial__card">
-            <i className='bx bxs-quote-alt-left blockquote__icon'></i>
-            <blockquote className="testimonial__blockquote">
-              <p>TaxPal is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.</p>
-            </blockquote>
-            <div className="testimonial__info">
-              <div>
-                <h2>Loretta Johnson</h2>
-                <p>Director at Acme</p>
+            <div className="testimonial__card">
+              <i className='bx bxs-quote-alt-left blockquote__icon'></i>
+              <blockquote className="testimonial__blockquote">
+                <p>TaxPal is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.</p>
+              </blockquote>
+              <div className="testimonial__info">
+                <div>
+                  <h2>Loretta Johnson</h2>
+                  <p>Director at Acme</p>
+                </div>
+                <img src="image/avatars/avatar-4.png" alt="Loretta Johnson" />
               </div>
-              <img src="image/avatars/avatar-4.png" alt="Loretta Johnson" />
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-}
-
-export default Testimonials;
+      </section>
+    );
+  }
+  
+  export default Testimonials;
